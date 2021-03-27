81c5e65a4ecccf69fe1b8430a763c0631bc2a6b4
Arman <armanhadi728@yahoo.com>
2021-03-27 10:51:15 +0430

    adding core app with urls

--- lifebot/lifebot/settings.py
+++ lifebot/lifebot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5i$%)lk=(%pp6j6ds3n(1-h!6px@uzvjfdm7_5*fc--n&z@@@k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'armanahdi.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
 ]
 
 MIDDLEWARE = [
