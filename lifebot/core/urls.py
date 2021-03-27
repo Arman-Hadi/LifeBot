e0b052edf008e218e3ffc33990ea9e2b12a600e0
Arman <armanhadi728@yahoo.com>
2021-03-27 12:37:06 +0430

    for tel

--- lifebot/core/urls.py
+++ lifebot/core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from .views import telegram_webhook, secret
+
+
 app_name = 'core'
 
 urlpatterns = [
-    
+    path(f'{secret}/', telegram_webhook, name='telegram-webhook')
 ]
