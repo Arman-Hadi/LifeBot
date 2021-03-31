acf6fd5a52e76fa02cf970d9a2c98a4f52759ae0
Arman <armanhadi728@yahoo.com>
2021-03-31 12:47:54 +0430

    tehran tz

--- lifebot/core/admin.py
+++ lifebot/core/admin.py
@@ -1,3 +1,5 @@
+import pytz
+
 from django.contrib import admin
 from django.utils import timezone
 
@@ -5,56 +7,68 @@ from .models import Question, QuestionType, Answer, TelUser
 
 
 def elapsed(date):
-        elapsed = timezone.now() - date
-        minute, second = divmod(elapsed.seconds, 60)
-        hour, minute = divmod(minute, 60)
-        year, day = divmod(elapsed.days, 365)
-        if year > 0:
-            return f'{year}y, {day}d'
-        elif minute > 0:
-            return f'{day}d, {hour}h, {minute}m'
-        else:
-            return f'Just Now!'
+    elapsed = timezone.now() - date
+    minute, second = divmod(elapsed.seconds, 60)
+    hour, minute = divmod(minute, 60)
+    year, day = divmod(elapsed.days, 365)
+    if year > 0:
+        return f'{year}y, {day}d'
+    elif minute > 0:
+        return f'{day}d, {hour}h, {minute}m'
+    else:
+        return f'Just Now!'
 
 
 @admin.register(TelUser)
 class TelUserAdmin(admin.ModelAdmin):
     list_display = ('chat_id', 'first_name', 'last_name',
-        'tel_username', 'date_joined', 'elapsed_time')
+        'tel_username', 'in_tehran', 'elapsed_time', 'date_joined')
     search_fields = ('first_name', 'chat_id', 'last_name', 'tel_username')
     list_filter = ('date_joined',)
 
     def elapsed_time(self, obj):
         return elapsed(obj.date_joined)
 
+    def in_tehran(self, obj):
+        return obj.date_joined.astimezone(tz=pytz.timezone('Asia/Tehran'))
+
 
 @admin.register(QuestionType)
 class QuestionTypeAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'date_updated', 'elapsed_time')
+    list_display = ('pk', 'name', 'in_tehran', 'elapsed_time', 'date_updated')
     search_fields = ('name', 'text')
     list_filter = ('date_updated',)
 
     def elapsed_time(self, obj):
         return elapsed(obj.date_updated)
 
+    def in_tehran(self, obj):
+        return obj.date_updated.astimezone(tz=pytz.timezone('Asia/Tehran'))
+
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'qtype', 'message_id','date_asked',
-        'elapsed_time')
+    list_display = ('message_id', 'qtype', 'in_tehran',
+        'elapsed_time', 'date_asked')
     search_fields = ('qtype', 'message_id')
     list_filter = ('date_asked',)
 
     def elapsed_time(self, obj):
         return elapsed(obj.date_asked)
 
+    def in_tehran(self, obj):
+        return obj.date_asked.astimezone(tz=pytz.timezone('Asia/Tehran'))
+
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
     list_display = ('pk', 'answer', 'user', 'question',
-        'answer_date', 'elapsed_time')
+        'in_tehran', 'elapsed_time', 'answer_date')
     search_fields = ('answer', 'user', 'question')
     list_filter = ('answer_date',)
 
     def elapsed_time(self, obj):
         return elapsed(obj.answer_date)
+
+    def in_tehran(self, obj):
+        return obj.answer_date.astimezone(tz=pytz.timezone('Asia/Tehran'))
